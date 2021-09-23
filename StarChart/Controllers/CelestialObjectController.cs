abffe0778fcb9464043b2ae3314e9e696cccf82e
Nehemiah Campbell <nehemiah.campbell@endjin.com>
2021-09-23 14:04:50 +0100

    Create CelestialObject Model: done

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -7,7 +7,9 @@ using StarChart.Data;
 
 namespace StarChart.Controllers
 {
-    public class CelestialObjectController : ControllerBase
+    [Route("")]
+    [ApiController]
+    public class CelestialObjectController : ControllerBase 
     {
         private readonly ApplicationDbContext _context;
 
